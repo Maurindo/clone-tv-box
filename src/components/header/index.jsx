ff2f3fcb293fb9c6e5ce8dc6b50aa393db602c8a
Maurindo Junior <maurindoapgjunior@gmail.com>
2024-02-04 17:58:14 -0300

    colocando as horas

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,43 @@
 import styles from './header.module.css'
+import React, {useState, useEffect} from 'react'
 
 export default function HeaderBox(){
+
+   const [segundos, setSegundos] = useState(new Date().getSeconds())
+
+   useEffect(() => {
+        const intervalo = setInterval(() => {
+            setSegundos(new Date().getSeconds())
+        }, 1000);
+
+        return () => clearInterval(intervalo)
+   }, []);
+
+   const horas = () =>{
+        let dados = new Date()
+        let dadosHoras = dados.getHours()
+
+        return dadosHoras
+   }
+
+   const minutos = () => {
+    let dados = new Date()
+    let minutoDados = dados.getMinutes()
+
+    return minutoDados
+   }
+
     return(
        <header className={styles.headerBox}>
             <div>
                 wi-fi
             </div>
             <div>
-                teste
+                <span>
+                    {horas()}:
+                    {minutos()}
+                    {segundos}
+                </span>
             </div>
        </header>
     );
